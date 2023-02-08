b195998fe244a5ca1458ed19f04363d3f4b7da61
korolevdp <korolevdp-228@yandex.ru>
2023-02-08 23:31:13 +0400

    Add Random numbers

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,4 @@
-﻿int number1 = 3;
-int number2 = 77;
+﻿int number1 = new Random().Next(1,10); //1,2,3,4, ...9
+int number2 = new Random().Next(1,10);
+Console.WriteLine( $"Number1 = {number1} \n Number2 = {number2}");
 Console.WriteLine($"Summ = {number1 + number2}");
